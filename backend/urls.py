0f71ec6b48897632489731a4cdcdeb552c2ad57c
Azim Islam <10747417+AzimIslam@users.noreply.github.com>
2023-08-04 14:35:26 +0100

    Fixed circular import error

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import *
+from . import views
 
 
-urlspatterns = [
-    path('addTask', addTask),
+urlpatterns = [
+    path('addTask/', views.addTask),
 ]
