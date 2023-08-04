b6d074cf97b2d70b1ec4e770bfe734b70d09ad68
Azim Islam <10747417+AzimIslam@users.noreply.github.com>
2023-08-04 14:37:55 +0100

    Added Due field

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class ListEntry(models.Model):
     description = models.CharField(max_length=100)
-    finished = models.BooleanField(default=False)
+    due = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.description
